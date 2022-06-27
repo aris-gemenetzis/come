acd4c99f3376ddfed4c11787eefe2614fd2b957f
ar3saris <85511857+ar3saris@users.noreply.github.com>
2022-06-27 13:59:17 +0300

    Update cython_utils.py

--- cython_utils.py
+++ cython_utils.py
@@ -1,5 +1,11 @@
 __author__ = 'ando'
 from distutils.core import setup
+try:
+    from setuptools import setup
+    from setuptools import Extension
+except ImportError:
+    from distutils.core import setup
+    from distutils.extension import Extension
 from Cython.Build import cythonize
 import numpy
 
