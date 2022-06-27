81c1efd529543c0d751a86bcaa4b84cdf578646b
ar3saris <85511857+ar3saris@users.noreply.github.com>
2022-06-27 14:04:33 +0300

    Update graph_utils.py

--- utils/graph_utils.py
+++ utils/graph_utils.py
@@ -1,7 +1,7 @@
 __author__ = 'ando'
 
 import numpy as np
-from time import time
+from time import time, perf_counter
 import logging as log
 import random
 
@@ -10,12 +10,67 @@ from itertools import zip_longest
 from scipy.io import loadmat
 from scipy.sparse import issparse
 
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 from multiprocessing import cpu_count
 from os import path
-from collections import Counter
+from collections import Counter, defaultdict
+from datetime import timedelta
 
 log.basicConfig(format='%(asctime).19s %(levelname)s %(filename)s: %(lineno)s %(message)s', level=log.INFO)
+
+
+# my function
+def compute_probabilities(G):
+    # log.info('Parsed {} edges with {} chunks in {}s'.format(total, idx, t1-t0))
+    p = 1
+    q = 1
+    log.info('computing probabilities atm')
+    global probs  # making probs global dONT ASK
+    probs = defaultdict(dict)
+    for node in G.nodes():
+        probs[node]['probabilities'] = dict()
+        probs[node]['neighbours'] = dict()
+
+    # new code
+    for source_node in G.nodes():
+        for current_node in G.neighbors(source_node):
+            probs_ = list()
+            neighs_ = list()
+            for destination in G.neighbors(current_node):
+
+                if source_node == destination:  # return parameter
+                    prob_ = G[current_node][destination].get('weight', 1) * (1 / p)
+                elif destination in G.neighbors(source_node):  # distance of one
+                    prob_ = G[current_node][destination].get('weight', 1)
+                else:  # in-out parameter
+                    prob_ = G[current_node][destination].get('weight', 1) * (1 / q)
+
+                probs_.append(prob_)
+                neighs_.append(destination)
+            probs[source_node]['probabilities'][current_node] = probs_ / np.sum(probs_)
+            probs[current_node]['neighbours'] = neighs_
+
+            # Save neighbors time_edges
+            neighbor2times = {}
+            for neighbor in neighs_:
+                neighbor2times[neighbor] = []
+                if 'time' in G[current_node][neighbor]:
+                    neighbor2times[neighbor].append(G[current_node][neighbor]['time'])
+                else:
+                    for att in list(G[current_node][neighbor].values()):
+                        # if 'time' not in att:
+                            # raise 'no time attribute'
+                        neighbor2times[neighbor].append(att['time'])
+            probs[current_node]['neighbours'] = neighbor2times
+
+    # print to file
+    # with open('./data/probs.txt', 'w') as test:
+        # print(probs, file=test)
+    log.info('finished computing probabilities atm')
+    # log.info(probs[0]['probabilities'][1])
+    # log.info(probs)
+    # log.info(probs['48']['neighbours']['13'])
+    return probs
 
 
 def __random_walk__(G, path_length, start, alpha=0, rand=random.Random()):
@@ -29,19 +84,32 @@ def __random_walk__(G, path_length, start, alpha=0, rand=random.Random()):
     :return:
     '''
 
-    path = [start]
-
-
-    while len(path) < path_length:
-        cur = path[-1]
-        if len(G.neighbors(cur)) > 0:
-            if rand.random() >= alpha:
-                path.append(rand.choice(G.neighbors(cur)))
-            else:
-                path.append(path[0])
-        else:
+    walk_len = path_length
+    walk = [start]
+    walk_options = list(G[start])
+    if len(walk_options) == 0:
+        return walk
+    first_step = np.random.choice(walk_options)
+    walk.append(first_step)
+    for k in range(walk_len - 2):
+        walk_options = list(G[walk[-1]])
+        if len(walk_options) == 0:
             break
-    return path
+        probabilities = probs[walk[-2]]['probabilities'][walk[-1]]
+        next_step = np.random.choice(walk_options, p=probabilities)
+        walk.append(next_step)
+    return walk
+
+    # while len(path) < path_length:
+        # cur = path[-1]
+        # if len(G.neighbors(cur)) > 0:
+            # if rand.random() >= alpha:
+                # path.append(rand.choice(G.neighbors(cur)))
+            # else:
+                # path.append(path[0])
+        # else:
+            #
+    # return path
 
 
 def __parse_adjacencylist_unchecked__(f):
@@ -85,7 +153,7 @@ def load_adjacencylist(file_, undirected=False, chunksize=10000):
     #read the matrix file
     t0 = time()
     with open(file_, 'r') as f:
-        with ProcessPoolExecutor(max_workers=cpu_count()) as executor:
+        with ThreadPoolExecutor(max_workers=cpu_count()) as executor:
             total = 0
             for idx, adj_chunk in enumerate(executor.map(parse_func, grouper(int(chunksize), f))): #execute pare_function on the adiacent list of the file in multipe process
                 adjlist.extend(adj_chunk) #merge the results of different process
@@ -134,20 +202,20 @@ def write_walks_to_disk(G, filebase, num_paths, path_length, alpha=0, rand=rando
     __current_graph = G
     __vertex2str = {v:str(v) for v in G.nodes()}
     files_list = ["{}.{}".format(filebase, str(x)) for x in range(num_paths)]
-    expected_size = len(G)
     args_list = []
     files = []
     log.info("file_base: {}".format(filebase))
+    probs = compute_probabilities(G)  # my code
     if num_paths <= num_workers:
-        paths_per_worker = [1 for x in range(num_paths)]
+        paths_per_worker = [1 for _ in range(num_paths)]
     else:
         paths_per_worker = [len(list(filter(lambda z: z!= None, [y for y in x]))) for x in grouper(int(num_paths / num_workers)+1, range(1, num_paths+1))]
 
-    with ProcessPoolExecutor(max_workers=num_workers) as executor:
+    with ThreadPoolExecutor(max_workers=num_workers) as executor:
         for size, file_, ppw in zip(executor.map(count_lines, files_list), files_list, paths_per_worker):
             args_list.append((ppw, path_length, alpha, random.Random(rand.randint(0, 2**31)), file_))
 
-    with ProcessPoolExecutor(max_workers=num_workers) as executor:
+    with ThreadPoolExecutor(max_workers=num_workers) as executor:
         for file_ in executor.map(_write_walks_to_disk, args_list):
             files.append(file_)
 
@@ -166,37 +234,22 @@ def count_lines(f):
     else:
         return 0
 
-def build_deepwalk_corpus(G, num_paths, path_length, alpha=0, rand=random.Random(0)):
-    '''
-    extract the walks form the graph used for context embeddings
-    :param G: graph
-    :param num_paths: how many random walks to form a sentence
-    :param path_length: how long each path -> length of the sentence
-    :param alpha: restart probability
-    :param rand: random function
-    :return:
-    '''
-    walks = []
-    nodes = list(G.nodes())
-    for cnt in range(num_paths):
-        rand.shuffle(nodes)
-        for node in nodes:
-            walks.append(__random_walk__(G, path_length, rand=rand, alpha=alpha, start=node))
-    return np.array(walks)
-
 
 def build_deepwalk_corpus_iter(G, num_paths, path_length, alpha=0, rand=random.Random(0)):
-    walks = []
     nodes = list(G.nodes())
     for cnt in range(num_paths):
+        # if cnt == 0:  # my code
+            # test_walk = __random_walk__(G,path_length, rand=rand, alpha=alpha, start=nodes[0])
+            # log.info('test_walk {}'.format(test_walk))
+            # log.info('lol {}'.format(probs[0]['probabilities'][1]))
         rand.shuffle(nodes)
         for node in nodes:
-            yield __random_walk__(G,path_length, rand=rand, alpha=alpha, start=node)
+            yield __random_walk__(G, path_length, rand=rand, alpha=alpha, start=node)
 
 
 def count_textfiles(files, workers=1):
     c = Counter()
-    with ProcessPoolExecutor(max_workers=workers) as executor:
+    with ThreadPoolExecutor(max_workers=workers) as executor:
         for c_ in executor.map(count_words, files):
             c.update(c_)
     return c
@@ -214,6 +267,25 @@ def count_words(file):
             words = [int(word) for word in l.strip().split()]
             c.update(words)
     return c
+
+# my code (load function)
+def load_edge_file(file):
+    p = 1
+    G = nx.Graph()
+    with open(file) as f:
+        for line in f:
+            u, v, w, t = line.split()
+            if G.has_edge(u, v):
+                if t not in G[u][v]['time']:
+                    G[u][v]['time'].append(int(t))
+            else:
+                G.add_edge(u, v, weight=float(w), time=[int(t)])
+    print(type(t), type(w))
+    print('graph', type(G['2']['141'].get('weight', 1)))
+    print('time',G['2']['141'].get('time', 1))
+    # print('test', G['2']['141'])
+    print(type(G))
+    return G
 
 def load_matfile(file_, variable_name="network", undirected=True):
   mat_varables = loadmat(file_)
