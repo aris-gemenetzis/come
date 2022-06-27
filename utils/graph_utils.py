8cdd852f12d44037ce631936028f463e87273977
ar3saris <85511857+ar3saris@users.noreply.github.com>
2022-06-27 14:18:49 +0300

    Update graph_utils.py

--- utils/graph_utils.py
+++ utils/graph_utils.py
@@ -317,4 +317,3 @@ def from_numpy(x, undirected=True):
 def grouper(n, iterable, padvalue=None):
     "grouper(3, 'abcdefg', 'x') --> ('a','b','c'), ('d','e','f'), ('g','x','x')"
     return zip_longest(*[iter(iterable)]*n, fillvalue=padvalue)
-
